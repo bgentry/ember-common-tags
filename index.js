2fa501e1e299d7128f48c7c67ef98b2b752801de
Blake Gentry <blakesgentry@gmail.com>
2017-05-01 11:03:59 -0700

    add broccoli-plugin

--- index.js
+++ index.js
@@ -2,5 +2,28 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-common-tags'
+  name: 'ember-common-tags',
+};
+
+/* eslint-env node */
+('use strict');
+
+module.exports = {
+  name: 'ember-common-tags',
+
+  included() {
+    this._super.included.apply(this, arguments);
+
+    this.import('vendor/-common-tags-bundle.js');
+    this.import('vendor/-common-tags-shims.js');
+  },
+
+  treeForVendor() {
+    const WebpackDependencyPlugin = require('./lib/webpack-dependency-plugin');
+
+    return new WebpackDependencyPlugin({
+      outputName: 'common-tags',
+      expose: ['common-tags'],
+    });
+  },
 };
